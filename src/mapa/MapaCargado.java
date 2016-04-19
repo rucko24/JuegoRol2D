9169ff84edfd6ffa0c447347655721a6e51eaf81
telekom <rucko24@gmail.com>
2016-04-19 02:08:07 -0430

    mapaCargado sucessfull

--- src/mapa/MapaCargado.java
+++ src/mapa/MapaCargado.java
@@ -50,11 +50,47 @@ public class MapaCargado extends Mapa {
 			switch (pixeles[i]) {
 			// cada color corresponde a un sprite
 			case 0x5a5a5a:
-				cuadrosCatalogo[i] = Cuadro.ASFALTO;
-				// continue para saltar del switch case, y saltar
+				cuadrosCatalogo[i] = Cuadro.ASFALTO;//
+				// continue para saltar del switch case, y pasar
 				// a la siguiente iteracion
 				continue;
-
+			case 0xffefb6:
+				cuadrosCatalogo[i] = Cuadro.ARENA;//
+				continue;
+			case 0xa6a4a3:
+				cuadrosCatalogo[i] = Cuadro.PIEDRA;
+				continue;
+			case 0x68505a:
+				cuadrosCatalogo[i] = Cuadro.OXIDO;//
+				continue;
+			case 0x857e70:
+				cuadrosCatalogo[i] = Cuadro.PIEDRA_ARENA_ASFALTO;// d2d2cb
+				continue;
+			case 0x2b2416:
+				cuadrosCatalogo[i] = Cuadro.BORDE_ASFALTO;
+				continue;
+			case 0xd2d2cb:
+				cuadrosCatalogo[i] = Cuadro.RALLA_ASFALTO;//
+				continue;
+			case 0xb3ac99:
+				cuadrosCatalogo[i] = Cuadro.ESQUINA_ASFALTO;//
+				continue;
+			case 0x7c1541:
+				cuadrosCatalogo[i] = Cuadro.PUERTA_CENTRAL_IZQUIERDA;//
+				continue;
+			case 0x850038:
+				cuadrosCatalogo[i] = Cuadro.PUERTA_SUPERIOR;//
+				continue;
+			case 0xff006c:
+				cuadrosCatalogo[i] = Cuadro.PUERTA_ESQUINA_SUPERIOR_IZUIERDA;//
+				continue;
+			case 0xffaa25:
+				cuadrosCatalogo[i] = Cuadro.PIEDRA_ARENA;//
+				continue;
+			case 0x524736:
+				cuadrosCatalogo[i] = Cuadro.PUERTA_CENTRAL;//
+			default:
+				cuadrosCatalogo[i] = Cuadro.VACIO; // cuadro negro
 			}
 		}
 	}
