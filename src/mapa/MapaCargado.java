a0675bee0b85e69d1fbf35a0506e78dc90a0282b
telekom <rucko24@gmail.com>
2016-04-19 01:24:07 -0430

    instanciando cuadrosCatalogo

--- src/mapa/MapaCargado.java
+++ src/mapa/MapaCargado.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
+import mapa.cuadro.Cuadro;
+
 public class MapaCargado extends Mapa {
 
 	// crear un array de pixles para almacenar el color de ellos
@@ -21,9 +23,17 @@ public class MapaCargado extends Mapa {
 
 	// flecha verde indica que sobreEscribimos
 	protected void cargarMapa(String ruta) {
-
+		// leer la imagen de nuestro mapa con este metodo
 		try {
 			BufferedImage imagen = ImageIO.read(MapaCargado.class.getResource(ruta));
+
+			ancho = imagen.getWidth();
+			alto = imagen.getHeight();
+			// Con esto inicializamos el ancho y alto del mapa
+
+			cuadrosCatalogo = new Cuadro[ancho * alto];// array de cuadros
+			pixeles = new int[ancho * alto]; // array de int
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
